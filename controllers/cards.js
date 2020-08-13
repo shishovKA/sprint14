767414dc4023474a3974e15beca22fa7469018cf
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-08-13 14:11:40 +0300

    fixed like/dislike 200 null problem

--- controllers/cards.js
+++ controllers/cards.js
@@ -31,8 +31,11 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((card) => {
+      if (!card) return res.status(404).send({ message: 'карточка не найдена' });
+      return res.send({ data: card });
+    })
+    .catch(() => res.status(404).send({ message: 'карточка не найдена' }));
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -42,6 +45,9 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((card) => {
+      if (!card) return res.status(404).send({ message: 'карточка не найдена' });
+      return res.send({ data: card });
+    })
+    .catch(() => res.status(404).send({ message: 'карточка не найдена' }));
 };
