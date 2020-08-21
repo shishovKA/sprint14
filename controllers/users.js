e18dfc6df1023981b35e1ccf79e3bc56e6225b6f
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-08-21 14:58:38 +0300

    modify createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user.js');
 
 module.exports.getUsers = (req, res) => {
@@ -17,8 +18,21 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(400).send({ message: err.message }));
 };
